ff482bc80ef74477e49c6560d66d84af8037d8a3
佐々木　謙 <nkekisasa222@gmail.com>
2022-01-29 15:18:37 +0900

    feat: WeatherDataStoreに天気情報を取得するコードを書いた

--- WeatherWidget/DataStore/WeatherDataStore.swift
+++ WeatherWidget/DataStore/WeatherDataStore.swift
@@ -6,3 +6,20 @@
 //
 
 import Foundation
+
+final class WeatherDataStore {
+    private let baseUrl = "https://api.openweathermap.org/data/2.5/"
+    private let shared = URLSession.shared
+    private let decoder = JSONDecoder()
+    
+    func fetchWeathers(params: String) async throws -> WeatherResponseModel {
+        let urlString = baseUrl + "onecall?lat=35.65146&lon=139.63678&exclude=current,minutely,daily,alerts&lang=ja&appid=\(API_KEY)"
+        guard let url = URL(string: urlString) else {
+            throw NSError(domain: "Error fetch weathers.", code: -1)
+        }
+        let request = URLRequest(url: url)
+        let (data, _) = try await shared.data(for: request)
+        let response = try decoder.decode(WeatherResponseModel.self, from: data)
+        return response
+    }
+}
