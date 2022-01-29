de4033d5b4f57308963cc33c202110ba41fe3db8
佐々木　謙 <nkekisasa222@gmail.com>
2022-01-29 12:04:55 +0900

    Initial Commit

---
+++ WeatherWidget/WeatherWidgetApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherWidgetApp.swift
+//  WeatherWidget
+//
+//  Created by sasaki.ken on 2022/01/29.
+//
+
+import SwiftUI
+
+@main
+struct WeatherWidgetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
