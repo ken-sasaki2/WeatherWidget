064539d26f470561a62dbd89aa5138fcc553d544
佐々木　謙 <nkekisasa222@gmail.com>
2022-01-29 15:17:56 +0900

    refactor: 今回必要とするデータだけを取得

--- WeatherWidget/Model/WeatherResponseModel.swift
+++ WeatherWidget/Model/WeatherResponseModel.swift
@@ -14,23 +14,10 @@ struct WeatherResponseModel: Decodable {
 struct Hourly: Decodable {
     var dt: Double
     var temp: Double
-    var feels_like: Double
     var pressure: Double
-    var humidity: Double
-    var dew_point: Double
-    var uvi: Double
-    var clouds: Double
-    var visibility: Double
-    var wind_speed: Double
-    var wind_deg: Double
-    var wind_gust: Double
     var weather: [Weather]
-    var pop: Double
 }
 
 struct Weather: Decodable {
-    var id: Double
     var main: String
-    var description: String
-    var icon: String
 }
