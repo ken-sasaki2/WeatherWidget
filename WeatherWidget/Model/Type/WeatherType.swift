84471cd3592974031b6aea853b60b869904c944c
佐々木　謙 <nkekisasa222@gmail.com>
2022-02-05 19:29:17 +0900

    add: TranslatorとTypeを追加

---
+++ WeatherWidget/Model/Type/WeatherType.swift
@@ -0,0 +1,15 @@
+//
+//  WeatherType.swift
+//  WeatherWidget
+//
+//  Created by sasaki.ken on 2022/02/05.
+//
+
+import Foundation
+
+enum WeatherType: String {
+    case Clear
+    case Clouds
+    case Rain
+    case Snow
+}
