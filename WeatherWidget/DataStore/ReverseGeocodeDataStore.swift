3bcffaf642a131d50d6bafb412ee67d00a43af5b
佐々木　謙 <nkekisasa222@gmail.com>
2022-02-05 20:31:35 +0900

    feat: 緯度経度から地点名取得

---
+++ WeatherWidget/DataStore/ReverseGeocodeDataStore.swift
@@ -0,0 +1,31 @@
+//
+//  ReverseGeocodeDataStore.swift
+//  WeatherWidget
+//
+//  Created by sasaki.ken on 2022/02/05.
+//
+
+import CoreLocation
+
+class ReverseGeocodeDataStore {
+    
+    func fetchLocationFromLatLng(lat: Double, lng: Double, onSuccess: @escaping(String) -> Void, onFailure: @escaping() -> Void){
+        let location = CLLocation(latitude: lat, longitude: lng)
+        CLGeocoder().reverseGeocodeLocation(location) { placemarks, error in
+            if let error = error {
+                print("Error fetch location from lat lng.")
+                print("Error message:", error)
+                print("Error locarized message:", error.localizedDescription)
+                onFailure()
+            }
+            
+            guard let locality = placemarks?.first?.locality else {
+                print("Error placemarks not found.")
+                onFailure()
+                return
+            }
+            
+            onSuccess(locality)
+        }
+    }
+}
