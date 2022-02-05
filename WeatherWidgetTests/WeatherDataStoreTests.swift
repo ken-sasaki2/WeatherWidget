9ffba8004d548cb986a62eaa8c59436c2c264f93
佐々木　謙 <nkekisasa222@gmail.com>
2022-02-05 20:09:00 +0900

    refactor: テストファイル名変更

--- WeatherWidgetTests/WeatherDataStoreTests.swift
+++ WeatherWidgetTests/WeatherDataStoreTests.swift
@@ -1,5 +1,5 @@
 //
-//  WeatherWidgetTests.swift
+//  WeatherDataStoreTests.swift
 //  WeatherWidgetTests
 //
 //  Created by sasaki.ken on 2022/01/29.
@@ -7,7 +7,7 @@
 
 import XCTest
 
-class WeatherWidgetTests: XCTestCase {
+class WeatherDataStoreTests: XCTestCase {
 
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
