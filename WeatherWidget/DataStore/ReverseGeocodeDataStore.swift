b3a2db67f58de26dfb5dc546db4f4c3840f7b5f0
佐々木　謙 <nkekisasa222@gmail.com>
2022-02-06 14:30:21 +0900

    refactor: ReverseGeocodeDataStoreをリファクタ

--- WeatherWidget/DataStore/ReverseGeocodeDataStore.swift
+++ WeatherWidget/DataStore/ReverseGeocodeDataStore.swift
@@ -9,23 +9,28 @@ import CoreLocation
 
 class ReverseGeocodeDataStore {
     
-    func fetchLocationFromLatLng(lat: Double, lng: Double, onSuccess: @escaping(String) -> Void, onFailure: @escaping() -> Void){
+    func fetchLocationFromLatLng(lat: Double, lng: Double, complication: @escaping(ReverseGeocodeResponce) -> Void) {
+        var responce = ReverseGeocodeResponce()
         let location = CLLocation(latitude: lat, longitude: lng)
+        
         CLGeocoder().reverseGeocodeLocation(location) { placemarks, error in
             if let error = error {
                 print("Error fetch location from lat lng.")
                 print("Error message:", error)
                 print("Error locarized message:", error.localizedDescription)
-                onFailure()
+                responce.error = error
+                complication(responce)
             }
             
             guard let locality = placemarks?.first?.locality else {
                 print("Error placemarks not found.")
-                onFailure()
+                responce.error = error
+                complication(responce)
                 return
             }
             
-            onSuccess(locality)
+            responce.location = locality
+            complication(responce)
         }
     }
 }
---
+++ WeatherWidget/Model/ReverseGeocodeResponce.swift
@@ -0,0 +1,13 @@
+//
+//  ReverseGeocodeResponce.swift
+//  WeatherWidget
+//
+//  Created by sasaki.ken on 2022/02/06.
+//
+
+import Foundation
+
+struct ReverseGeocodeResponce {
+    var location: String?
+    var error: Error?
+}
