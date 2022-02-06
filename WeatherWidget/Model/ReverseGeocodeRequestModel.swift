a513b80688b4db475c4085f66e643c509998ae2e
佐々木　謙 <nkekisasa222@gmail.com>
2022-02-06 16:16:55 +0900

    add: ReverseGeocodeRequestModelを追加

---
+++ WeatherWidget/Model/ReverseGeocodeRequestModel.swift
@@ -0,0 +1,13 @@
+//
+//  ReverseGeocodeRequestModel.swift
+//  WeatherWidget
+//
+//  Created by sasaki.ken on 2022/02/06.
+//
+
+import Foundation
+
+struct ReverseGeocodeRequestModel {
+    var lat: Double
+    var lng: Double
+}
