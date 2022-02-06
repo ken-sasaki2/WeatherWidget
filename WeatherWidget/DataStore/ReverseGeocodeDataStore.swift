5421c083dc4ded23168e617efb18df23cf921ab2
佐々木　謙 <nkekisasa222@gmail.com>
2022-02-06 23:03:12 +0900

    refactor: ReverseGeocodeDataStoreをasync/awaitで書き換え

--- WeatherWidget/DataStore/ReverseGeocodeDataStore.swift
+++ WeatherWidget/DataStore/ReverseGeocodeDataStore.swift
@@ -9,23 +9,25 @@ import CoreLocation
 
 class ReverseGeocodeDataStore {
     
-    func fetchLocationFromLatLng(lat: Double, lng: Double, onSuccess: @escaping(String) -> Void, onFailure: @escaping() -> Void) {
-        let location = CLLocation(latitude: lat, longitude: lng)
-        CLGeocoder().reverseGeocodeLocation(location) { placemarks, error in
-            if let error = error {
-                print("Error fetch location from lat lng.")
-                print("Error message:", error)
-                print("Error locarized message:", error.localizedDescription)
-                onFailure()
+    func fetchLocationFromLatLng(requestModel: ReverseGeocodeRequestModel) async throws -> ReverseGeocodeResponceModel {
+        let location = CLLocation(latitude: requestModel.lat, longitude: requestModel.lng)
+        
+        return try await withCheckedThrowingContinuation { continuation in
+            CLGeocoder().reverseGeocodeLocation(location) { placemarks, error in
+                if let error = error {
+                    print("Error fetch location from lat lng.")
+                    print("Error message:", error)
+                    print("Error locarized message:", error.localizedDescription)
+                    continuation.resume(throwing: error)
+                }
+                
+                if let locality = placemarks?.first?.locality {
+                    let responce = ReverseGeocodeResponceModel(location: locality)
+                    continuation.resume(returning: responce)
+                } else {
+                    continuation.resume(throwing: NSError(domain: "Error locality not found.", code: -2))
+                }
             }
-            
-            guard let locality = placemarks?.first?.locality else {
-                print("Error placemarks not found.")
-                onFailure()
-                return
-            }
-            
-            onSuccess(locality)
         }
     }
 }
