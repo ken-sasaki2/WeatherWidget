c8293e1d17d0a5387f495a750996dbf599b0593f
佐々木　謙 <nkekisasa222@gmail.com>
2022-02-08 21:58:20 +0900

    feat: 時刻バナー作成

--- Widget/View/MediumWidgetView.swift
+++ Widget/View/MediumWidgetView.swift
@@ -13,15 +13,37 @@ struct MediumWidgetView: View {
     var entry: MediumWidgetProvider.Entry
     
     var body: some View {
+        let backgroundColor = colorScheme == .dark ? Color.black : Color.white
         let hourlyWeathers = entry.hourlyWeathers
-        Text("Hello, World!")
+        let timePeriodTexts = entry.timePeriodTexts
+        
+        GeometryReader { geometry in
+            let geometryWidth = geometry.size.width
+            let geometryHeight = geometry.size.height
+            let widthPerHour = (geometryWidth - 32) / 24 // 1時間あたりのwidth
+            VStack {
+                HStack(alignment: .center, spacing: 0) {
+                    ForEach(timePeriodTexts, id: \.self) { timePeriodText in
+                        Text(timePeriodText)
+                            .foregroundColor(.black)
+                            .font(.system(size: 15, weight: .medium))
+                            .fixedSize(horizontal: true, vertical: true)
+                            .frame(width: widthPerHour, alignment: .center)
+                    }
+                }
+                .frame(width: geometryWidth - 32, height: geometryHeight - 32)
+                .clipped()
+            }
+            .frame(width: geometryWidth, height: geometryHeight)
+        }
+        .background(backgroundColor)
     }
 }
 
 struct MediumWidgetView_Previews: PreviewProvider {
     static var previews: some View {
         let entry = MediumWidgetEntryModel(
-            currentDate: Date(timeIntervalSince1970: 1644044400),
+            currentDate: Date(timeIntervalSince1970: 1644048000),
             hourlyWeathers: MockHourly.data,
             currentLocation: "世田谷区"
         )
