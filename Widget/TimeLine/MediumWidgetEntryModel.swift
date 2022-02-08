602dd6b13afd65d7f5bc2842cdd86899c00bd5dc
佐々木　謙 <nkekisasa222@gmail.com>
2022-02-08 16:26:38 +0900

    update: 天気情報を8時間に絞る。天気アイコン専用配列を作成しておく。

--- Widget/TimeLine/MediumWidgetEntryModel.swift
+++ Widget/TimeLine/MediumWidgetEntryModel.swift
@@ -12,10 +12,42 @@ struct MediumWidgetEntryModel: TimelineEntry {
     let date: Date
     var hourlyWeathers: [Hourly]
     var currentLocation: String
+    var weatherIcons: [String]
     
     init(currentDate: Date, hourlyWeathers: [Hourly], currentLocation: String) {
         self.date = currentDate
-        self.hourlyWeathers = hourlyWeathers
         self.currentLocation = currentLocation
+        
+        self.hourlyWeathers = []
+        self.weatherIcons = []
+        
+        // 現在時刻から8時間分にhourlyWeathersを絞る
+        for index in 0..<8 {
+            self.hourlyWeathers.append(hourlyWeathers[index])
+            
+            let weather = self.hourlyWeathers[index].weather[index]
+            if let weatherName = getWeatherName(weather: weather) {
+                self.weatherIcons.append(weatherName)
+            }
+        }
+    }
+    
+    private func getWeatherName(weather: Weather) -> String? {
+        var wetherName: String
+        
+        switch weather.main {
+        case "Clear":
+            wetherName = WeatherTypeTranslator.translate(type: .Clear)
+        case "Clouds":
+            wetherName = WeatherTypeTranslator.translate(type: .Clouds)
+        case "Rain":
+            wetherName = WeatherTypeTranslator.translate(type: .Rain)
+        case "Snow":
+            wetherName = WeatherTypeTranslator.translate(type: .Snow)
+        default:
+            return nil
+        }
+        
+        return wetherName
     }
 }
