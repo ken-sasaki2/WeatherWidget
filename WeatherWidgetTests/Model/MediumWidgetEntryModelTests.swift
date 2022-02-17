ee796c67ae53662937055655f1a8f6e7e36f4676
佐々木　謙 <nkekisasa222@gmail.com>
2022-02-17 17:51:21 +0900

    add: MediumWidgetEntryModelをテストするファイルの追加

---
+++ WeatherWidgetTests/Model/MediumWidgetEntryModelTests.swift
@@ -0,0 +1,23 @@
+//
+//  MediumWidgetEntryModelTests.swift
+//  WeatherWidgetTests
+//
+//  Created by sasaki.ken on 2022/02/17.
+//
+
+import XCTest
+
+class MediumWidgetEntryModelTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        
+    }
+}
