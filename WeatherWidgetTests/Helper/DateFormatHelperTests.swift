f65811b1f2e0505112568200d01a5fdb5207cdd8
佐々木　謙 <nkekisasa222@gmail.com>
2022-02-17 17:47:33 +0900

    test: DateFormatHelperのテスト書いた

--- WeatherWidgetTests/Helper/DateFormatHelperTests.swift
+++ WeatherWidgetTests/Helper/DateFormatHelperTests.swift
@@ -8,6 +8,8 @@
 import XCTest
 
 class DateFormatHelperTests: XCTestCase {
+    private let morningTime = Date(timeIntervalSince1970: 1645056000) // 2022年2月17日 木曜日 09:00:00
+    private let nightTime = Date(timeIntervalSince1970: 1645099200) // 2022年2月17日 木曜日 21:00:00
 
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
@@ -18,10 +20,24 @@ class DateFormatHelperTests: XCTestCase {
     }
 
     func testFormatToHHmm() throws {
-        
+        XCTContext.runActivity(named: "時刻が09:00の場合") { _ in
+            let result = DateFormatHelper.shared.formatToHHmm(date: morningTime)
+            XCTAssertEqual("09:00", result)
+        }
+        XCTContext.runActivity(named: "時刻が21:00の場合") { _ in
+            let result = DateFormatHelper.shared.formatToHHmm(date: nightTime)
+            XCTAssertEqual("21:00", result)
+        }
     }
     
-    func formatToHH() throws {
-        
+    func testFormatToHH() throws {
+        XCTContext.runActivity(named: "時刻が09:00の場合") { _ in
+            let result = DateFormatHelper.shared.formatToHH(date: morningTime)
+            XCTAssertEqual("09", result)
+        }
+        XCTContext.runActivity(named: "時刻が21:00の場合") { _ in
+            let result = DateFormatHelper.shared.formatToHH(date: nightTime)
+            XCTAssertEqual("21", result)
+        }
     }
 }
