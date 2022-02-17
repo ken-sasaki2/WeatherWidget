034df6914697a3aa1c092ad38d51ecbd60009a39
佐々木　謙 <nkekisasa222@gmail.com>
2022-02-17 17:33:59 +0900

    add: DateFormatHelperTests追加

---
+++ WeatherWidgetTests/Helper/DateFormatHelperTests.swift
@@ -0,0 +1,27 @@
+//
+//  DateFormatHelperTests.swift
+//  WeatherWidgetTests
+//
+//  Created by sasaki.ken on 2022/02/17.
+//
+
+import XCTest
+
+class DateFormatHelperTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testFormatToHHmm() throws {
+        
+    }
+    
+    func formatToHH() throws {
+        
+    }
+}
